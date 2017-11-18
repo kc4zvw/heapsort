ae0010b4f20b98426cf81fcfadeb112a4bf5761c
David Billsbrough <kc4zvw@www.kc4zvw.org>
2017-11-18 01:17:32 -0500

    improved comments

--- heapsort.py
+++ heapsort.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python2
 #
-# $Id: heapsort.py,v 0.37 2017/10/31 15:19:21 kc4zvw Exp kc4zvw $
+# $Id: heapsort.py,v 0.39 2017/11/18 06:15:49 kc4zvw Exp kc4zvw $
 
-# Favorite:
+# Purpose:
 #  I am trying to figue out how to make the following Code for Heap
 #  sort work with many data types:
 
 import os
 
-def swap(i, j):                    
-	wordlist[i], wordlist[j] = wordlist[j], wordlist[i] 
-
-def heapify(end, i):   
-	l = 2 * i + 1  
-	r = 2 * (i + 1)   
-	max = i   
-	if l < end and wordlist[i] < wordlist[l]:   
-		max = l   
-	if r < end and wordlist[max] < wordlist[r]:   
-		max = r   
-	if max != i:   
-		swap(i, max)   
-		heapify(end, max)   
-
-def heapsort():     
-	end = len(wordlist)   
-	start = end // 2 - 1 # use // instead of /
-	for i in range(start, -1, -1):   
-		heapify(end, i)   
-	for i in range(end-1, 0, -1):   
-		swap(i, 0)   
-		heapify(i, 0)   
-	return wordlist
 
 ### ====================================================================
+###  Demonstation of the heap sort using multiple data types
+### ====================================================================
+
+def swap(i, j):
+	wordlist[i], wordlist[j] = wordlist[j], wordlist[i] 
+
+def heapify(end, i):
+	l = 2 * i + 1  
+	r = 2 * (i + 1)
+	max = i
+	if l < end and wordlist[i] < wordlist[l]:
+		max = l
+	if r < end and wordlist[max] < wordlist[r]:
+		max = r
+	if max != i:
+		swap(i, max)
+		heapify(end, max)
+
+def heapsort():
+	end = len(wordlist)
+	start = end // 2 - 1 # use // instead of /
+	for i in range(start, -1, -1):
+		heapify(end, i)
+	for i in range(end-1, 0, -1):
+		swap(i, 0)
+		heapify(i, 0)
+	return wordlist
+
+### --------------------------------------------------------------------
 
 def get_home_directory():
 	home = os.getenv("HOME")
@@ -41,19 +46,23 @@ def get_home_directory():
 	return home
 
 def print_header(str):
-	n = len(str)
-	print("\n" + ' ' + str)
-	print('-' * (n + 2))
+	n = len(str) + 2
+	print("\n %s" % str)
+	print('-' * n)
 
 def open_text_file():
-    
+
 	myHome = get_home_directory()
-	text_file = myHome + os.sep + 'alphabet.txt'
+	text_file = myHome + os.sep + 'Text' + os.sep + 'alphabet.txt'
 	words = []
 
 	print("Reading text file : %s" % text_file)
 
-	f = open(text_file, 'r')
+	try:
+		f = open(text_file, 'r')
+	except:
+		print('Error: file not found : %s' % text_file)
+		os.exit(1)
 
 	for a in f.readlines():
 		a = a.rstrip("\n")
@@ -64,6 +73,8 @@ def open_text_file():
 
 	return words
 
+### ====================================================================
+##					Main Program
 ### ====================================================================
 
 numlist = [2, 7, 1, -2, 56, 5, 3]
@@ -80,9 +91,7 @@ for num in listing:
 	print("%10d" % num)
 print("\n")
 
-### ====================================================================
-
-''' Main Program '''
+### --------------------------------------------------------------------
 
 del wordlist[:]
 
@@ -100,7 +109,7 @@ for char in listing:
 	print("%9c" % char)
 print("\n")
 
-### ====================================================================
+### --------------------------------------------------------------------
 
 unsorted = []
 unsorted = open_text_file()
